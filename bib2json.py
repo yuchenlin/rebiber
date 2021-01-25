c527afd4cb70a80cce7a6f4bf0d8416494b67e36
Yang Q <xiaoyanq@usc.edu>
2021-01-25 17:13:41 +0800

    update bib2json.py and normalize.py

--- bib2json.py
+++ bib2json.py
@@ -1,12 +1,13 @@
 import json
 import re
+import sys
  
 def normalize_title(title_str):
     title_str = re.sub(r'[^a-zA-Z]',r'', title_str) 
     return title_str.lower().replace(" ", "").strip()
 
 
-def load_bib_file(bibpath="anthology.bib"):
+def load_bib_file(bibpath="acl.bib"):
     all_bib_entries = []
     with open(bibpath) as f:
         bib_entry_buffer = []
@@ -21,17 +22,21 @@ def load_bib_file(bibpath="anthology.bib"):
 def buil_json(all_bib_entries):
     all_bib_dict = {}
     for bib_entry in all_bib_entries:
-        assert bib_entry[1].strip().startswith("title =")
-        title = bib_entry[1].replace('    title = "', "").replace('",', "")
-        # print(title) 
-        bib_key = normalize_title(title)
-        all_bib_dict[bib_key] = bib_entry
+        for entry in bib_entry:
+            if entry.strip().startswith("title"):
+                target_bp = entry.find('=')+1
+                while entry[target_bp] == ' ':
+                    target_bp += 1
+                title = entry.replace(entry[:target_bp], "").replace('",', "")
+                # print(title) 
+                bib_key = normalize_title(title)
+                all_bib_dict[bib_key] = bib_entry
+                break
     return all_bib_dict
 
 
 if __name__ == "__main__":
-    all_bib_entries = load_bib_file("anthology.bib")
+    all_bib_entries = load_bib_file(sys.argv[1])
     all_bib_dict = buil_json(all_bib_entries)
-    with open("anthology.json", "w") as f:
+    with open(sys.argv[2], "w") as f:
         json.dump(all_bib_dict, f, indent=2)
-
--- normalize.py
+++ normalize.py
@@ -1,27 +1,44 @@
 from bib2json import normalize_title, load_bib_file
 import argparse
 import json
+import sys
 
-with open("anthology.json") as f:
-    acl_db = json.load(f)
-all_bib_entries = load_bib_file("example_input.bib")
+def normalize_bib(bib_db, all_bib_entries):
+    output_bib_entries = []
+    log_text = ""
+    for bib_entry in all_bib_entries:
+        # read the title from this bib_entry
+        original_title = ""
+        original_bibkey = ""
+        for entry in bib_entry:
+            if entry.strip().startswith("@"):
+                key_start_idx = entry.find('{')+1
+                key_end_idx = len(entry)-2
+                original_bibkey = entry[key_start_idx:key_end_idx].strip()
+            if entry.strip().startswith("title"):
+                start_idx = entry.find('=')+1
+                while not entry[start_idx].isalpha():
+                    start_idx += 1
+                end_idx = len(entry)-1
+                while not entry[end_idx].isalpha():
+                    end_idx -= 1
+                original_title = entry[start_idx:end_idx+1]
+                break
+        title = normalize_title(original_title)    
+        # try to map the bib_entry to the keys in all_bib_entries
+        if title in bib_db:
+            output_bib_entries.append(bib_db[title])
+            log_str = "Converted to the official format. ID: %s ; Title: %s" % (original_bibkey, original_title)
+            print(log_str)
+            log_text += log_str
+        else:
+            output_bib_entries.append(bib_entry)
+            
+    # TODO: write the log_text to a file 
 
 
-output_bib_entries = []
-log_text = ""
-for bib_entry in all_bib_entries:
-    # read the title from this bib_entry
-    original_title = ""
-    original_bibkey = ""
-    title = normalize_title(original_title)    
-    # try to map the bib_entry to the keys in all_bib_entries
-    if title in all_bib_entries:
-        output_bib_entries.append(all_bib_entries[title])
-        log_str = "Converted to the official format. ID: %s ; Title: %s" % (original_bibkey, original_title)
-        print(log_str)
-        log_text += log_str
-    else:
-        output_bib_entries.append(bib_entry)
-        
-# TODO: write the log_text to a file 
-
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        bib_db = json.load(f)
+    all_bib_entries = load_bib_file(sys.argv[2])
+    normalize_bib(bib_db, all_bib_entries)
