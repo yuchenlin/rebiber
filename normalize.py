a8f10b680bb32e69c078fe07f90453ea0daeb35b
yuchenlin <yuchen.lin@usc.edu>
2021-01-25 21:10:30 -0800

    remove the comments

--- normalize.py
+++ normalize.py
@@ -24,23 +24,6 @@ def normalize_bib(args, bib_db, all_bib_entries):
         # read the title from this bib_entry
         original_title = ""
         original_bibkey = ""
-        # for entry_idx in range(len(bib_entry)):
-        #     entry = bib_entry[entry_idx]
-        #     if entry.strip().startswith("@"):
-        #         original_bibkey = entry[entry.find('{')+1:-1]
-        #         if not original_bibkey:
-        #             # the bib id is in the second line
-        #             original_bibkey = bib_entry[entry_idx+1].strip()[:-1]
-        #             print(original_bibkey)
-        #     if entry.strip().lower().startswith("title"):
-        #         start_idx = entry.find('=')+1
-        #         while not entry[start_idx].isalpha():
-        #             start_idx += 1
-        #         end_idx = len(entry)-1
-        #         while not entry[end_idx].isalpha():
-        #             end_idx -= 1
-        #         original_title = entry[start_idx:end_idx+1]
-        #         break
         bib_entry_str = " ".join([line for line in bib_entry if "month" not in line.lower()]).lower()
         bib_entry_parsed = bibtexparser.loads(bib_entry_str)
         if "title" not in bib_entry_parsed.entries[0]:
