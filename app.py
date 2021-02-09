dbba8810a187666bc13009efc1184f53468a6e4d
Yang Q <xiaoyanq@usc.edu>
2021-02-10 00:36:13 +0800

    add upload functionality

--- app.py
+++ app.py
@@ -1,64 +1,67 @@
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, render_template, url_for, request, redirect, send_from_directory
+import os
+from werkzeug.utils import secure_filename
+import rebiber
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
+filepath = os.path.dirname(os.path.abspath(__file__)) + '/'
+app.config["UPLOAD_FOLDER"] = filepath + "static/uploads"
+app.config["STATIC_FOLDER"] = filepath + 'static/'
+app.config["ALLOWED_EXTENSIONS"] = ["bib"]
 
 
-@app.route('/', methods=['POST', 'GET'])
+def process_file(input_file_path, bib_list):
+    bib_db = rebiber.construct_bib_db(bib_list, app.config["STATIC_FOLDER"])
+    all_bib_entries = rebiber.load_bib_file(input_file_path)
+    output_path = input_file_path
+    rebiber.normalize_bib(bib_db, all_bib_entries, output_path)
+
+
+def allowed_file(filename):
+    # We only want files with a . in the filename
+    if not "." in filename:
+        return False
+
+    # Split the extension from the filename
+    ext = filename.rsplit(".", 1)[1]
+
+    # Check if the extension is in ALLOWED_IMAGE_EXTENSIONS
+    if ext.lower() in app.config["ALLOWED_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+    if request.method == "POST":
+        
+        if request.files:
+            bib_file = request.files["bib"]
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+            if bib_file.filename == "":
+                print("No filename")
+                return redirect(request.url)
 
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+            if allowed_file(bib_file.filename):
+                filename = secure_filename(bib_file.filename)
+                bib_file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+                print("File uploaded successfully")
+                process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), os.path.join(app.config["STATIC_FOLDER"], "bib_list.txt"))
+                return redirect(url_for('uploaded_file', filename=filename))
+                # return redirect(request.url)
+            
+            else:
+                print("That file extension is not allowed")
+                return redirect(request.url)
+
+    return render_template("index.html")
 
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+   return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 
 if __name__ == "__main__":
