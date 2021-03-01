64d2321241d391a94536190b7555beed90a60707
Yang Q <xiaoyanq@usc.edu>
2021-03-01 21:19:46 +0800

    add deduplicate functionality

--- app.py
+++ app.py
@@ -9,36 +9,14 @@ app = Flask(__name__)
 filepath = os.path.dirname(os.path.abspath(__file__)) + '/'
 app.config["UPLOAD_FOLDER"] = filepath
 app.config["ALLOWED_EXTENSIONS"] = ["bib"]
-# BUCKET = "rebiber"
-# os.environ['AWS_PROFILE'] = "Profile1"
 
 
-# def upload_file(file_name, bucket):
-#     """
-#     Function to upload a file to an S3 bucket
-#     """
-#     object_name = file_name
-#     s3_client = boto3.client('s3')
-#     response = s3_client.upload_file(file_name, bucket, object_name)
-
-#     return response
-
-
-# def download_bib(file_name, bucket):
-#     """
-#     Function to download a given file from an S3 bucket
-#     """
-#     s3 = boto3.resource('s3')
-#     s3.Bucket(bucket).download_file(file_name, file_name)
-#     return file_name
-
-
-def process_file(input_file_path):
+def process_file(input_file_path, deduplicate):
     all_bib_entries = rebiber.load_bib_file(input_file_path)
     filepath = os.path.abspath(rebiber.__file__).replace("__init__.py","")
     bib_list_path = os.path.join(filepath, "bib_list.txt")
     bib_db = rebiber.construct_bib_db(bib_list_path, start_dir=filepath)
-    rebiber.normalize_bib(bib_db, all_bib_entries, "output.bib") 
+    rebiber.normalize_bib(bib_db, all_bib_entries, "output.bib", deduplicate=deduplicate) 
 
 
 def allowed_file(filename):
@@ -73,8 +51,13 @@ def index():
                 bib_file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
                 print("File uploaded successfully")
 
-                process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                # upload_file(f"output.bib", BUCKET)
+                # check if any checkbox is selected
+                deduplicate = True
+                if request.form.get('duplicate'):
+                    print('keep duplicate is selected')
+                    deduplicate = False
+
+                process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), deduplicate)
                 return redirect('/downloadfile/'+ 'output.bib')
             
             else:
@@ -93,14 +76,7 @@ def download_file(filename):
 
 @app.route('/return-files/<filename>')
 def return_files_tut(filename):
-    # output = download_bib(f"{filename}", BUCKET)
-    # return send_file(output, as_attachment=True)
-    return send_file(f"{filename}", as_attachment=True)
-
-
-# @app.route('/back')
-# def back():
-#     return render_template("index.html")
+    return send_file(f"{filename}", as_attachment=True, cache_timeout=-1)
 
 
 if __name__ == "__main__":
